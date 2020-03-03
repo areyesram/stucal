1a058fb91acefab534d591ff1978ad663fcc5534
areyesram <aryeg1hu>
2020-03-02 23:29:29 -0600

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace StupidCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtFormula = new System.Windows.Forms.TextBox();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtFormula);
+            this.panel1.Controls.Add(this.btnResult);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(509, 56);
+            this.panel1.TabIndex = 2;
+            // 
+            // txtFormula
+            // 
+            this.txtFormula.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtFormula.Location = new System.Drawing.Point(0, 0);
+            this.txtFormula.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFormula.Multiline = true;
+            this.txtFormula.Name = "txtFormula";
+            this.txtFormula.Size = new System.Drawing.Size(434, 56);
+            this.txtFormula.TabIndex = 5;
+            // 
+            // btnResult
+            // 
+            this.btnResult.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnResult.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResult.Location = new System.Drawing.Point(434, 0);
+            this.btnResult.Margin = new System.Windows.Forms.Padding(0);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(75, 56);
+            this.btnResult.TabIndex = 4;
+            this.btnResult.Text = "=";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblResult.Location = new System.Drawing.Point(0, 56);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(509, 41);
+            this.lblResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 97);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Stupid Calc";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtFormula;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Microsoft.CSharp;
+
+namespace StupidCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var expression = CoerceNumbers(txtFormula.Text);
+                lblResult.Text = CalculateResult(expression).ToString(CultureInfo.CurrentCulture);
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = ex.Message;
+            }
+        }
+
+        private static string CoerceNumbers(string expression)
+        {
+            var numericValue = new Regex(@"([\d\.]+)");
+            var allDigits = new Regex(@"^\d+$");
+            var sb = new StringBuilder();
+            foreach (var s in numericValue.Split(expression))
+            {
+                sb.Append(s);
+                if (allDigits.IsMatch(s)) sb.Append(".0");
+            }
+            return sb.ToString();
+        }
+
+        private static double CalculateResult(string expression)
+        {
+            var parameters = new CompilerParameters {GenerateInMemory = true, GenerateExecutable = false};
+            var code = Resource.ClassTemplate.Replace("{EXPR}", expression);
+            var assembly = new CSharpCodeProvider().CompileAssemblyFromSource(parameters, code);
+            if (assembly.Errors.HasErrors)
+            {
+                var sb = new StringBuilder();
+                foreach (CompilerError error in assembly.Errors)
+                    sb.AppendLine(error.ErrorText);
+                throw new InvalidOperationException(sb.ToString());
+            }
+            var result = assembly
+                .CompiledAssembly.GetType("StupidCalc.Calculator")
+                .GetMethod("GetResult")
+                .Invoke(null, null);
+            return (double) result;
+        }
+    }
+}
